9a16335c35bb688d575c6067d1069ea562130517
Karston Kuciemba <kuciembakarston@gmail.com>
2023-09-20 16:58:46 -0500

    fixed population

--- server/data_utils.py
+++ server/data_utils.py
@@ -2,6 +2,6 @@ import random
 def generate_data():
     """Creates a list with 500 elements of a random value (5-750)"""
     my_list = []
-    for _ in range(500):
-        my_list.append(random.randint(5, 750))
+    for _ in range(175):
+        my_list.append(random.randint(5, 550))
     return my_list
