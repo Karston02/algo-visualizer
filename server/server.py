261141a267ef5230c8dbb952c693778ef117ac75
Karston Kuciemba <kuciembakarston@gmail.com>
2023-10-06 18:03:51 -0500

    fixed changing array

--- server/server.py
+++ server/server.py
@@ -2,17 +2,21 @@ from flask import Flask, jsonify
 import data_utils as du
 
 app = Flask(__name__)
+bars = None  # Initialize bars to None
 
 @app.route('/graph', methods=['GET'])
 def get_graph():
-    bars = du.generate_data()
+    global bars
+    bars = du.generate_data()  # Call generate_data only if bars is None
     return jsonify(bars)
 
 @app.route('/merge-sort', methods=['GET'])
 def merge_sort():
-    bars = du.generate_data()  # Get the same array as in get_graph
-    du.merge_sort(bars)  # Sort the bars in-place using merge_sort from data_utils
-    return jsonify(bars)
+    global bars
+    # should never be None, but just in case
+    if bars is not None:
+        du.merge_sort(bars)  # sort the existing bars in-place
+    return jsonify(bars)  # return the sorted bars
 
 if __name__ == '__main__':
     app.run(debug=True)
