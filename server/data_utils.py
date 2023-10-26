b01c442392a94e0e1653de31f7e1540bb9676e64
Karston Kuciemba <kuciembakarston@gmail.com>
2023-10-26 18:03:43 -0500

    added bubble sort

--- server/data_utils.py
+++ server/data_utils.py
@@ -6,52 +6,75 @@ def generate_data():
         my_my_list.append(random.randint(5, 550))
     return my_my_list
 
-def merge_sort(my_list, animations=None):
+def merge_sort(my_list, steps=[]):
     """Sorts a my_list using the merge sort algorithm. This algorithm is recursive and splits
     the my_list into two halves until each my_list has a single element. Then, the algorithm
     compares the two my_lists and merges them into a single my_list in sorted order. It is commonly 
     referred to as a divide and conquer algorithm."""
-    if animations is None:
-        animations = []  # Create an empty my_list to store the animations
-
     if len(my_list) > 1:
-        left_my_list = my_list[:len(my_list)//2]
-        right_my_list = my_list[len(my_list)//2:]
+        left_list = my_list[:len(my_list)//2]
+        right_list = my_list[len(my_list)//2:]
 
         # Recursive call on these. It will call until the my_list has len 1.
-        merge_sort(left_my_list, animations)
-        merge_sort(right_my_list, animations)
+        merge_sort(left_list)
+        merge_sort(right_list)
 
         i = 0  # Left index
         j = 0  # Right index
         k = 0  # Merged index
 
-        while i < len(left_my_list) and j < len(right_my_list):
-            if left_my_list[i] < right_my_list[j]:
+        while i < len(left_list) and j < len(right_list):
+            if left_list[i] < right_list[j]:
                 # If the leftmost element in the left my_list is smaller, add it to the merged my_list.
-                my_list[k] = left_my_list[i]
+                my_list[k] = left_list[i]
                 i += 1
             else:
                 # If the leftmost element in the right my_list is smaller, add it to the merged my_list.
-                my_list[k] = right_my_list[j]
+                my_list[k] = right_list[j]
                 j += 1
 
             # Append the indices of the two elements being compared at this step.
-
             k += 1
+        steps.append(my_list.copy())
 
         # If the left my_list has elements left.
-        while i < len(left_my_list):
-            my_list[k] = left_my_list[i]
+        while i < len(left_list):
+            my_list[k] = left_list[i]
             i += 1
             k += 1
+            steps.append(my_list.copy())
 
             # Append the index for the left my_list element.
 
         # If the right my_list has elements left.
-        while j < len(right_my_list):
-            my_list[k] = right_my_list[j]
+        while j < len(right_list):
+            my_list[k] = right_list[j]
             j += 1
             k += 1
+            steps.append(my_list.copy())
+    return my_list, steps  # Return the my_list of steps
 
-    return my_list, steps  # Return the my_list of animations
+def bubble_sort(my_list, steps=None):
+    """Sorts a my_list using the bubble sort algorithm. This algorithm compares two adjacent elements
+    and swaps them if they are not in order. It will continue to iterate through the my_list until
+    no swaps are made."""
+    if steps is None:
+        steps = [my_list.copy()]
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(my_list) - 1):
+            if my_list[i] > my_list[i + 1]:
+                # Swap the elements
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+                swapped = True
+                steps.append(my_list.copy())
+    return steps
+
+
+my_list, my_steps = (merge_sort(generate_data()))
+print(my_steps)
+
+print("BUBBLE SORT START")
+my_steps = (bubble_sort(generate_data()))
+print(my_steps)
