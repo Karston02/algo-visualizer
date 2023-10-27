38dd3885fcc329ac2c60e39a6e66e1a8bbd30c23
Karston Kuciemba <kuciembakarston@gmail.com>
2023-10-26 21:04:57 -0500

    created own bubblesort file

---
+++ client/src/components/SortingFunctions/BubbleSort.ts
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import { SetStateAction } from 'react';
+
+// delay between each animation step (in ms)
+const BUBBLE_ANIMATION_INTERVAL = 10;
+
+export function startBubbleSortAnimation(setIsSorting: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }, setGraph: { (value: SetStateAction<never[]>): void; (arg0: any): void; }) {
+
+  setIsSorting(true);
+  axios
+    .get('/bubble-sort') // call the bubble sort API endpoint
+    .then((response) => {
+      // after call, retrieve the steps and animations from the response
+      const { steps, animations } = response.data;
+      animateBubbleSort(steps, animations); // animate the bubble sort
+    })
+    .catch((error) => {
+      // if there is an error, log it to the console and stop sorting
+      console.error('Error:', error);
+      setIsSorting(false); // stop sorting
+    });
+
+  /*
+  This function animates the bubble sort algorithm. It takes in the steps and animations
+  from the API response and iterates through them, updating the graph and highlighting
+  the compared elements at each step.
+  */
+  const animateBubbleSort = (steps: number[], animations: [number, number][]) => {
+    let stepIndex = 0; // will increment thru 2d list
+    const animationInterval = setInterval(() => {
+      // if we aren't at last index (final sorted list)
+      if (stepIndex < steps.length) {
+        const currentList = steps[stepIndex];
+        // update graph with current list
+        setGraph(currentList);
+
+        // retrieve elements being compared at current step
+        const [i, j] = animations[stepIndex];
+        if (i >= 0 && j >= 0) { // if not first call (-1, -1)
+          highlightComparedElements(i, j);
+        }
+        stepIndex += 1; // onto next step
+      } else { // if we are at last index
+        clearInterval(animationInterval);
+        setIsSorting(false); // done sorting
+      }
+    }, BUBBLE_ANIMATION_INTERVAL); // delay for cascade effect
+  };
+
+  // this function highlights the elements being compared at each step
+  const highlightComparedElements = (index1: number, index2: number) => {
+    // query DOM for bars and highlight the ones relating to our index
+    const bars = document.querySelectorAll('.bar');
+    bars[index1].classList.add('red-bar');
+    bars[index2].classList.add('red-bar');
+
+    // after delay, remove the highlighted class from those
+    setTimeout(() => {
+      bars[index1].classList.remove('red-bar');
+      bars[index2].classList.remove('red-bar');
+    }, BUBBLE_ANIMATION_INTERVAL);
+  };
+
+}
--- client/src/components/SortingVisualizer/SortingVisualizer.tsx
+++ client/src/components/SortingVisualizer/SortingVisualizer.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './sortingVisualizerStyles.css';
 import OptionBar from '../OptionBar/OptionBar';
 import axios from 'axios';
+import { startBubbleSortAnimation } from '../SortingFunctions/BubbleSort'
 
 function SortingVisualizer() {
   const [graph, setGraph] = useState([]);
@@ -36,60 +37,51 @@ function SortingVisualizer() {
     });
 };
 
-const startBubbleSortAnimation = () => {
+
+
+const startInsertionSortAnimation = () => {
   setIsSorting(true);
   axios
-    .get('/bubble-sort')
+    .get('/insertion-sort')
     .then((response) => {
       const { steps, animations } = response.data;
-      animateBubbleSort(steps, animations);
+      animateInsertionSort(steps, animations);
     })
     .catch((error) => {
       console.error('Error:', error);
     });
-};
+}
 
-const ANIMATION_INTERVAL = 50;
-const animateBubbleSort = (steps: string | any[], animations: [any, any][]) => {
-  let stepIndex = 0;
+const animateInsertionSort = (steps: string | any[], animations: [number, number][]) => {
+}
 
-  const animationInterval = setInterval(() => {
-    if (stepIndex < steps.length) {
-      const currentList = steps[stepIndex];
-      setGraph(currentList);
+const startQuickSortAnimation = () => {
+  setIsSorting(true);
+  axios
+    .get('/insertion-sort')
+    .then((response) => {
+      const { steps, animations } = response.data;
+      animateInsertionSort(steps, animations);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 
-      // Check if the animation values are greater than or equal to 0
-      const [i, j] = animations[stepIndex];
-      if (i >= 0 && j >= 0) { // skip first (-1, -1)
-        highlightComparedElements(i, j);
-      }
-      stepIndex += 1;
-    } else {
-      clearInterval(animationInterval);
-      setIsSorting(false);
-    }
-  }, ANIMATION_INTERVAL);
-};
-
-
-const highlightComparedElements = (index1: number, index2: number) => {
-  const bars = document.querySelectorAll('.bar'); // Assuming you have a class "bar" for each bar element
-  bars[index1].classList.add('red-bar'); // Add a class to highlight the first compared element
-  bars[index2].classList.add('red-bar'); // Add a class to highlight the second compared element
-
-  // delay to remove red-bar (KEEP SAME AS ANIMATION INTERVAL)
-  setTimeout(() => {
-    bars[index1].classList.remove('red-bar');
-    bars[index2].classList.remove('red-bar');
-  }, ANIMATION_INTERVAL); // Adjust the delay (in milliseconds) as needed
-};
+const animateQuickSort = (steps: string | any[], animations: [number, number][]) => {
+}
 
 
 
   return (
     <div className="page-container">
       <h1 className="main-header">Algorithm Visualizer</h1>
-      <OptionBar graph={graph} />
+      <OptionBar 
+      startBubbleSortAnimation={() => startBubbleSortAnimation(setIsSorting, setGraph)}
+      startMergeSortAnimation={startMergeSortAnimation} 
+      startInsertionSortAnimation={startInsertionSortAnimation} 
+      startQuickSortAnimation={startQuickSortAnimation}
+      />
       <div className="border">
         <div className="bar-container">
           {graph.map((value, index) => (
@@ -102,12 +94,6 @@ const highlightComparedElements = (index1: number, index2: number) => {
         </div>
       </div>
       <div className="button-container">
-        <button onClick={startMergeSortAnimation}>
-          MERGE TESTER
-        </button>
-        <button onClick={startBubbleSortAnimation}>
-          BUBBLE TESTER
-        </button>
         <button className="reset-button" onClick={generateNewData}>
           Reset Data
         </button>
