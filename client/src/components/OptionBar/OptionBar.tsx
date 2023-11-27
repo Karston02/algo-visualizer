8264673ad758811c23ad791ce5cadbb80eb6c854
Karston Kuciemba <kuciembakarston@gmail.com>
2023-11-27 12:24:58 -0600

    added interface

--- client/src/components/OptionBar/OptionBar.tsx
+++ client/src/components/OptionBar/OptionBar.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import './optionBarStyles.css';
+
+interface OptionBarProps {
+  startBubbleSortAnimation: () => void;
+  startMergeSortAnimation: () => void;
+  startQuickSortAnimation: () => void;
+  startInsertionSortAnimation: () => void;
+  startSelectionSortAnimation: () => void;
+}
+
 function OptionBar({ 
   startBubbleSortAnimation,
   startMergeSortAnimation,
   startQuickSortAnimation,
   startInsertionSortAnimation,
-  startSelectionSortAnimation
-}: {
-  startBubbleSortAnimation: () => void;
-  startMergeSortAnimation: () => void;
-  startQuickSortAnimation: () => void;
-  startInsertionSortAnimation: () => void;
-  startSelectionSortAnimation: () => void; }) {
+  startSelectionSortAnimation,
+}: OptionBarProps) {
   return (
     <div className="container">
-        <button className="merge-type-button" onClick={startMergeSortAnimation}>Merge Sort</button>
-        <button className="merge-type-button" onClick={startQuickSortAnimation}>Quick Sort</button>
-        <button className="merge-type-button" onClick={startInsertionSortAnimation}>Insertion Sort</button>
-        <button className="merge-type-button" onClick={startBubbleSortAnimation}>Bubble Sort</button>
-        <button className="merge-type-button" onClick={startSelectionSortAnimation}>Selection Sort</button>
+        <button className="merge-type-button" onClick={() => startMergeSortAnimation()}>Merge Sort</button>
+        <button className="merge-type-button" onClick={() => startQuickSortAnimation()}>Quick Sort</button>
+        <button className="merge-type-button" onClick={() => startInsertionSortAnimation()}>Insertion Sort</button>
+        <button className="merge-type-button" onClick={() => startBubbleSortAnimation()}>Bubble Sort</button>
+        <button className="merge-type-button" onClick={() => startSelectionSortAnimation()}>Selection Sort</button>
     </div>
   )
 }
