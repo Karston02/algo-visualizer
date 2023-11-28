0a7b1c88c6bc117bfaccabd742f3f8a2b784ec28
Karston Kuciemba <kuciembakarston@gmail.com>
2023-11-27 20:19:10 -0600

    temporarily removed animation

--- client/src/components/SortingFunctions/QuickSort.ts
+++ client/src/components/SortingFunctions/QuickSort.ts
@@ -6,41 +6,17 @@ const QUICK_ANIMATION_INTERVAL = 100;
 
 export function startQuickSortAnimation(setIsSorting: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }, setGraph: { (value: SetStateAction<never[]>): void; (arg0: any): void; }) {
 
-    setIsSorting(true);
-    axios
-        .get('/quick-sort')
-        .then((response) => {
-        const { steps, animations } = response.data;
-        animateQuickSort(steps, animations);
-        })
-        .catch((error) => {
-        console.error('Error:', error);
-        });
+  setIsSorting(true);
+  axios
+      .get('/quick-sort')
+      .then((response) => {
+      const { bars, steps } = response.data;
+      // animateMergeSort(animations, bars);
+      setGraph(bars);
+      })
+      .catch((error) => {
+      console.error('Error:', error);
+      });
       
-      const animateQuickSort = (steps: string | any[], animations: [number, number][]) => {
-        let stepIndex = 0;
-        const animationInterval = setInterval(() => {
-          if (stepIndex < steps.length) {
-            const currentList = steps[stepIndex];
-            setGraph(currentList);
-            const [i, j] = animations[stepIndex];
-            if (i >= 0 && j >= 0) {
-              highlightComparedElements(i, j);
-            }
-            stepIndex += 1;
-          } else {
-            clearInterval(animationInterval);
-            setIsSorting(false);
-          }
-        }, QUICK_ANIMATION_INTERVAL);
-      }
-      const highlightComparedElements = (index1: number, index2: number) => {
-        const bars = document.querySelectorAll('.bar');
-        bars[index1].classList.add('red-bar');
-        bars[index2].classList.add('red-bar');
-        setTimeout(() => {
-          bars[index1].classList.remove('red-bar');
-          bars[index2].classList.remove('red-bar');
-        }, QUICK_ANIMATION_INTERVAL);
-      }
+      
 }
